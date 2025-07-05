1434e6a7215dc9054882446e169d7d98ba55f5ad
garry <hunagqiannb@gmail.com>
2025-07-05 09:25:18 +0800

    update

---
+++ send_super_overtime.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+# @Time : 2025/5/29 18:01
+# @Author : Garry-Host
+# @FileName: send_file_tjh
+from prefect import task,flow
+import hashlib
+import time
+import uuid
+import requests
+import pandas as pd
+import json
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+from datetime import datetime,timedelta
+from utils import asbot
+from config import asbot_config
+
+# 获取寄修数据积压数据
+
+def get_session():
+    session = requests.Session()
+    retries = Retry(total=5, backoff_factor=0.5, status_forcelist=[502, 503, 504])
+    adapter = HTTPAdapter(max_retries=retries)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
+
+@task
+def generate_requrl(pageindex, page, day):
+    print(f"正在生成第{page}页的URL")
+    """
+    从 API 获取数据并转换为 DataFrame
+    """
+    # 基本参数
+
+    tenant = "laifen"
+    api_name = "api/vlist/ExecuteQuery"
+    timestamp = str(int(time.time() * 1000))
+    reqid = str(uuid.uuid1())
+    appid = "AS_department"
+    queryid = "38c53a54-813f-a0e0-0000-06f40ebdeca5"
+    is_user_query = "true"
+    is_preview = "false"
+    pagesize = "5000"
+    paging = "true"
+    key = "u7BDpKHA6VSqTScpEqZ4cPKmYVbQTAxgTBL2Gtit"
+    orderby = "createdon descending"
+    # extendConditions = f'[{{"name":"new_checkon","val":"{day}","op":"last-x-days"}},{"name":"new_deliveriedon","val":"null","op":"null"},{"name":"new_solution","val":30,"op":"eq"}]'
+    conditions = [
+        {"name": "new_checkon", "val": day, "op": "last-x-days"},
+        {"name": "new_deliveriedon", "val": 'null', "op": "null"},  # None 将自动转为 null
+        {"name": "new_solution", "val": 30, "op": "eq"}
+    ]
+
+    extendConditions = json.dumps(conditions, ensure_ascii=False)
+    args = [appid, extendConditions, orderby, pageindex, pagesize, paging, reqid, tenant, timestamp, is_preview,
+            is_user_query, queryid, key]
+
+    """
+    生成签名
+    """
+
+    sign_str = "".join(args)
+    sign = hashlib.sha256(sign_str.encode('utf-8')).hexdigest().upper()
+    # 构建 URL
+    url = (
+        f"https://ap6-openapi.fscloud.com.cn/t/{tenant}/open/{api_name}"
+        f"?$tenant={tenant}&$timestamp={timestamp}&$reqid={reqid}&$appid={appid}"
+        f"&queryid={queryid}&isUserQuery={is_user_query}&isPreview={is_preview}"
+        f"&$pageindex={pageindex}&$pagesize={pagesize}&$paging={paging}"
+        f"&$extendConditions={extendConditions}&$orderby={orderby}&$sign={sign}"
+    )
+    print(f"成功生成第{page}页的URL: {url}")
+    return url
+
+@task
+def fetch_api_data(url, page):
+    print(f"正在获取第{page}页数据")
+    # 发送 GET 请求
+    session = get_session()
+    response = session.get(url, timeout=30)
+    if response.status_code != 200:
+        raise Exception(f"API 请求失败，状态码: {response.status_code}")
+
+    # 解析 JSON 数据
+    data = response.json()
+    entities = data["Data"]["Entities"]
+
+    df = pd.DataFrame(entities)
+    print(f"第{page}页数据，已通过API获取成功获取")
+    return df
+
+@task
+def extract_need_data(df):
+    df = df.assign(
+        产品类型=df["new_productmodel_id"].apply(lambda x: x.get("name", None)),
+        产品名称=df["new_product_id"].apply(lambda x: x.get("name", None)),
+        旧件签收时间=df["FormattedValues"].apply(lambda x: x.get("new_signedon", None)),
+        检测时间=df["FormattedValues"].apply(lambda x: x.get("new_checkon", None)),
+        申请类别=df["FormattedValues"].apply(lambda x: x.get("new_srv_rma_0.new_applytype", None)),
+        一检时间=df["FormattedValues"].apply(lambda x: x.get("laifen_onechecktime", None)),
+        维修完成时间=df["FormattedValues"].apply(lambda x: x.get("laifen_servicecompletetime", None)),
+        质检完成时间=df["FormattedValues"].apply(lambda x: x.get("laifen_qualityrecordtime", None)),
+        单号=df['new_rma_id'].apply(lambda x: x.get('name', None)),
+        分拣人员=df['laifen_systemuser2_id'].apply(lambda x: x.get('name', None) if pd.notnull(x) else None),
+        处理状态=df["FormattedValues"].apply(lambda x: x.get("new_srv_rma_0.new_status", None)),
+        旧件处理状态=df["FormattedValues"].apply(lambda x: x.get("new_returnstatus", None)),
+        检测结果=df["FormattedValues"].apply(lambda x: x.get("new_solution", None)),
+        故障现象=df['new_error_id'].apply(lambda x: x.get('name', None) if pd.notnull(x) else None),
+        # 发货时间=df['new_deliveriedon'],
+        一检人员=df['laifen_systemuser_id'].apply(lambda x: x.get('name', None) if pd.notnull(x) else None),
+        发货状态=df['FormattedValues'].apply(lambda x: x.get('new_srv_rma_0.new_deliverstatus', None)),
+        产品序列号=df['new_userprofilesn'],
+        服务人员=df['new_srv_workorder_1.new_srv_worker_id'].apply(
+            lambda x: x.get('name', None) if pd.notnull(x) else None),
+        单据来源=df["FormattedValues"].apply(lambda x: x.get("new_srv_rma_0.new_fromsource", None)),
+        创建时间=df["FormattedValues"].apply(lambda x: x.get("createdon", None)),
+    )
+    #    # 选择需要的列
+    df = df[[
+        '单号', '产品类型', '产品名称', '处理状态', '旧件处理状态', '检测结果', '申请类别', '旧件签收时间',
+        '检测时间', '一检时间', '维修完成时间', '质检完成时间', '故障现象', '发货状态',
+        '一检人员', '产品序列号', '分拣人员', '服务人员', '单据来源', '创建时间'
+    ]]
+    print(f"成功提取所需数据,共{df.shape[1]}列")
+    return df
+
+@task
+def get_sf_data(days=15):
+    print(f"正在下载最近{days}天的数据")
+    pageindex = "1"
+    url = generate_requrl(pageindex, '0', days)
+
+    rs = requests.get(url)
+    count = rs.json()['Data']['TotalRecordCount']
+    print(f"最近{days}天签收业务量共{count}单,共{count // 5000 + 2}页数据")
+    datas = []
+
+    for i in range(1, count // 5000 + 2):
+        url = generate_requrl(str(i), i, days)
+        data = fetch_api_data(url, i)
+        print(f"第{i}页数据已获取")
+        datas.append(data)
+
+    df = pd.concat(datas, ignore_index=True)
+    df = extract_need_data(df)
+    print(f"已成功下载最近{days}天的数据")
+    return df
+
+
+@flow(name='发送寄修严重超时数据')
+def send_data():
+    t1 = datetime.today().date()
+    t2 = datetime(2025, 6, 1).date()
+    dt = (t1 - t2).days + 3
+    data = get_sf_data(dt)
+    
+    
+    with open(r"E:\Dev\myprefect\data\jx\over_power.json", "r", encoding="utf-8") as f:
+        bef = json.load(f)  # bef['yd'] 是上次的“昨日总量”
+
+
+    end_date1 = datetime.today().date() - timedelta(7)
+    end_date2 = datetime.today().date() - timedelta(6)
+
+    data['检测时间'] = pd.to_datetime(data['检测时间'])
+
+    print(end_date1,end_date2)
+    first = data[
+        (data['检测时间'] >= '2025-06-01') &
+        (data['检测时间'] <= str(end_date1)) &
+        (data['产品类型'].isin(['产成品-吹风机', '产成品-电动牙刷', '产成品-剃须刀']))
+    ]['单号'].count()
+
+    last = data[
+        (data['检测时间'] >= '2025-06-01') &
+        (data['检测时间'] <= str(end_date2)) &
+        (data['产品类型'].isin(['产成品-吹风机', '产成品-电动牙刷', '产成品-剃须刀']))
+    ]['单号'].count()
+    print(first,last)
+    less = bef['yd'] - first
+    more = last + less - bef['yd']
+
+    bef['yd'] = int(last)
+    with open(r"E:\Dev\myprefect\data\jx\over_power.json", "w", encoding="utf-8") as f:
+        json.dump(bef, f, ensure_ascii=False, indent=2)
+        
+    file_path = f"./data/jx/2025-06-01至{end_date2}分拣未发货数据明细.xlsx"
+    data[
+        (data['检测时间'] >= '2025-06-01') &
+        (data['检测时间'] <= str(end_date2)) &
+        (data['产品类型'].isin(['产成品-吹风机', '产成品-电动牙刷', '产成品-剃须刀']))
+    ].to_excel(file_path,index=False)
+
+    message = (f"截止{end_date2}号之前分拣未发货数据\n"
+               f"寄修严重超时机器共{last}台\n"
+               f"环比{end_date1}号之前(2530)，减少{less}台\n"
+               f"今日新增{more}台，净增长{more-less}台")
+    print(message)
+    bot = asbot.AsBot('人机')
+    bot.sendfile("xls", file_name=file_path.split('/')[-1], file_path=file_path,
+                   type_file=asbot_config.TYPE_FILE)
+    bot.send_text_to_group(message)
+    
+    
+if __name__ == '__main__':
+    # send_data()
+    bot = asbot.AsBot('人机')
+    pp ={
+            "zh_cn": {
+                "title": "消息标题",  # 消息标题
+                "content": [  # 消息内容，由多个段落组成
+                    [  # 第一个段落
+                        {
+                            "tag": "text",  # 文本元素
+                            "text": "这是一段文本内容"
+                        },
+                        {
+                            "tag": "a",  # 超链接元素
+                            "text": "点击这里",
+                            "href": "http://example.com"
+                        }
+                    ],
+                    [  # 第二个段落
+                        {
+                            "tag": "at",  # @用户元素
+                            "user_id": "ou_xxxxxx",  # 用户ID
+                            "user_name": "用户名"  # 可选，显示名称
+                        }
+                    ]
+                ]
+            }
+        }
+
+
+
+    bot.send_post_to_group(pp)
